76ccfa0d0306e7df051f307ee169babc2e40a416
a994947007 <994947007@qq.com>
2022-10-29 05:02:23 +0800

    unit test interval

--- rxAndroid/src/test/java/com/android/demo/rxandroid/ExampleUnitTest.java
+++ rxAndroid/src/test/java/com/android/demo/rxandroid/ExampleUnitTest.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.android.demo.rxandroid.observable.Observable;
+import com.android.demo.rxandroid.observer.Consumer;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +18,15 @@ public class ExampleUnitTest {
   @Test
   public void addition_isCorrect() {
     assertEquals(4, 2 + 2);
+
+    Observable.interval(100, TimeUnit.MILLISECONDS)
+            .subscribe(new Consumer<Long>() {
+              @Override
+              public void accept(Long aLong) {
+                System.out.println(aLong);
+              }
+            });
+
+    TimeUnit.SECONDS.sleep();
   }
 }
